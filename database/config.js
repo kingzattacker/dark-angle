4abc81b2e0b1ff142044080fc255c0bfd95c19bc
kingznverlose <ibrohimmoh7@gmail.com>
2026-01-21 13:23:05 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8465154707:AAHrapMYap_Ws9JQdUwE513SrDbbCWctEY0",  // Masukin Bot token kamu
+  owners: "7552335798", // Masukin ID Telegram kamu
+  port: "4000", // Masukin Port panel kamu 
+  ipvps: "http://server.lynz.biz.id" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1995-1996 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
